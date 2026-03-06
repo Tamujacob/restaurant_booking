34e1b5a8eeeaeb84f6c6a704e08c279c14517ea6
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-03-06 20:16:34 +0300

    Registered the Tablebooking table

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import TableBooking
 
-# Register your models here.
+@admin.register(TableBooking)
+class TableBookingAdmin(admin.ModelAdmin):
+    list_display  = ['full_name', 'email', 'phone', 'location', 'date', 'time', 'guests', 'status', 'created_at']
+    list_filter   = ['status', 'location', 'date']
+    search_fields = ['first_name', 'last_name', 'email', 'phone']
+    ordering      = ['-created_at']
+    list_editable = ['status']
