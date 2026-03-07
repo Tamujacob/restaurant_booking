f38ca1a7287afb9f108f3926ff08382267aba91c
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-03-07 21:50:16 +0300

    Update admin.py file

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TableBooking
+from .models import TableBooking, MenuItem
 
 @admin.register(TableBooking)
 class TableBookingAdmin(admin.ModelAdmin):
@@ -8,3 +8,10 @@ class TableBookingAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name', 'email', 'phone']
     ordering      = ['-created_at']
     list_editable = ['status']
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['cat', 'name', 'desc', 'price', 'badge', 'is_available']
+    list_filter = ['cat', 'is_available']
+    search_fields = ['name', 'cat']
+    list_editable = ['is_available']
