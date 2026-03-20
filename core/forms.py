72390fd02fa94701432085ec9e371d4ff3b42bf8
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-03-20 15:47:22 +0300

    Register Order in Forms

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import TableBooking
+from .models import TableBooking, Order
 
 class TableBookingForm(forms.ModelForm):
 
@@ -62,3 +62,56 @@ class TableBookingForm(forms.ModelForm):
                 'rows': 3,
             }),
         }
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model  = Order
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'delivery_location',
+            'date',
+            'delivery_time',
+        ]   
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Your first name',
+                'id': 'bookFirst',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Your last name',
+                'id': 'bookLast',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'you@email.com',
+                'id': 'bookEmail',
+            }),
+            'phone': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': '+256 700 000 000',
+                'id': 'bookPhone',
+            }),
+            'delivery_location': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Enter your delivery address',
+                'id': 'orderLocation',
+            }),
+            'date': forms.DateInput(attrs={
+                'class': 'form-input',
+                'type': 'date',
+                'id': 'bookDate',
+            }),
+            'delivery_time': forms.TimeInput(attrs={
+                'class': 'form-input',
+                'type': 'time',
+                'id': 'bookTime',
+            }),
+            
+        } 
