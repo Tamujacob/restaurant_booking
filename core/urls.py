dde7dc9417d3ea8bbfb9616da3f21c62f52b53d4
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-04-04 18:30:57 +0300

    add feedback and order URLs to urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.admin_login, name='login'),
-    path('order/', views.place_order, name='place_order')
+    path('order/', views.place_order, name='place_order'),
+    path('feedback/', views.submit_feedback, name='feed_back')
 ]
