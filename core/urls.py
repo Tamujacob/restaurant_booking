5ddeda6d25b3d23876523d62b7633eccedef0cee
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-04-10 21:33:35 +0300

    Add a star rating to the feedback

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.admin_login, name='login'),
     path('order/', views.place_order, name='place_order'),
-    path('feedback/', views.submit_feedback, name='feed_back')
+    path('feedback/', views.submit_feedback, name='submit_feedback')
 ]
