e5c761352e68a74f99978a9933430502e08a32e8
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-07-08 17:12:28 +0300

    Fix:Import TableBookingSerializer in views

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import (
     MenuItemSerializer, LocationSerializer,
-    OrderSerializer, FeedbackSerializer
+    OrderSerializer, FeedbackSerializer, TableBookingSerializer,
 )
 
 
