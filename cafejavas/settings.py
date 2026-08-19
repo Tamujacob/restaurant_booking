da31f513f3e2792102b84641d71a6e991770d9ba
TAMUKEDDE JACOB <jacobtamukedde@gmail.com>
2026-08-19 14:52:58 +0300

    feat: migrate database from SQLite to PostgreSQL

--- cafejavas/settings.py
+++ cafejavas/settings.py
@@ -1,9 +1,8 @@
-"""
-Django settings for cafejavas project.
-"""
 
 from pathlib import Path
 from decouple import config
+
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -93,12 +92,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cafejavas.wsgi.application'
 
 
-# Database — SQLite is fine for a progress demo
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600,
+    )
 }
 
 
